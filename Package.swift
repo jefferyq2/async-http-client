a34ca0fbaaf7a52e80367c2d6a54435f0d5b8dbd
Nghia Tran <vinhnghiatran@gmail.com>
2022-04-07 09:55:33 +0700

    Try using NIO from Proxyman

--- Package.swift
+++ Package.swift
@@ -21,10 +21,10 @@ let package = Package(
         .library(name: "AsyncHTTPClient", targets: ["AsyncHTTPClient"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-nio.git", from: "2.38.0"),
-        .package(url: "https://github.com/apple/swift-nio-ssl.git", from: "2.14.1"),
+        .package(url: "https://github.com/apple/swift-nio.git", from: "2.36.0"),
+        .package(url: "https://github.com/apple/swift-nio-ssl.git", from: "2.17.1-proxyman"),
         .package(url: "https://github.com/apple/swift-nio-http2.git", from: "1.19.0"),
-        .package(url: "https://github.com/apple/swift-nio-extras.git", from: "1.10.0"),
+        .package(url: "https://github.com/apple/swift-nio-extras.git", from: "1.10.2"),
         .package(url: "https://github.com/apple/swift-nio-transport-services.git", from: "1.11.4"),
         .package(url: "https://github.com/apple/swift-log.git", from: "1.4.0"),
     ],
