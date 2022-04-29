ada0c58b4c33deb8317b19b380fba838f671ff02
Nghia Tran <vinhnghiatran@gmail.com>
2022-04-29 09:19:36 +0700

    Use Proxyman packages

--- Package.swift
+++ Package.swift
@@ -22,7 +22,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-nio.git", from: "2.40.0"),
-        .package(url: "https://github.com/apple/swift-nio-ssl.git", from: "2.19.0-proxyman"),
+        .package(url: "https://github.com/ProxymanApp/swift-nio-ssl.git", from: "2.19.0-proxyman"),
         .package(url: "https://github.com/apple/swift-nio-http2.git", from: "1.19.0"),
         .package(url: "https://github.com/apple/swift-nio-extras.git", from: "1.10.2"),
         .package(url: "https://github.com/apple/swift-nio-transport-services.git", from: "1.11.4"),
